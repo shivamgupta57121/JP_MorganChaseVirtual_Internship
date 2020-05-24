225416b76dd044f0b6db28b7cf93b93a90cb16a3
SHIVAM GUPTA <31137037+shivamgupta57121@users.noreply.github.com>
2020-05-24 19:25:06 +0530

    Task 3

---
+++ JPMC-tech-task-3-PY3/src/DataManipulator.ts
@@ -0,0 +1,32 @@
+import { ServerRespond } from './DataStreamer';
+
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+  timestamp: Date,
+}
+
+
+export class DataManipulator {
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price)/2; 
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price)/2; 
+    const ratio = priceABC / priceDEF;
+    const upperbound = 1 + 0.05;
+    const lowerbound = 1 - 0.05;
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      upper_bound: upperbound,
+      lower_bound: lowerbound,
+      trigger_alert: (ratio > upperbound || ratio < lowerbound) ? ratio : undefined,
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ?  serverResponds[0].timestamp : serverResponds[1].timestamp,
+    };
+  }
+}
